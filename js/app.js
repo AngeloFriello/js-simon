cc7cb27c97223562bb07cb2d08bcbafcf042697b
angelo friello <angelofriello01@gmail.com>
2023-10-18 19:02:55 +0200

    add controllo

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
 
+const randomArray = getArrayRandom(1,100,5)
 
-const ramdomNumbers = alert('cerca di memorizzare il maggior numero possibile di numeri ' + getArrayRandom(1,100,5))
+const ramdomNumbers = alert('cerca di memorizzare il maggior numero possibile di numeri ' + randomArray)
 
+console.log(randomArray)
 
+let utentNumbersArray = [];
+let countSame = 0;
+let arraySame = [];
 
+// con questo ciclo formo l array di numeri 
+for(let i = 0; i < randomArray.length; i++){
+    n = i + 1
+    const utentNumbers = parseInt(prompt('inserisci i numeri che ti ricordi'));
+    console.log(utentNumbers);
+
+    utentNumbersArray.push(utentNumbers);
+
+    if(randomArray.includes(utentNumbers) && ! arraySame.includes(utentNumbers) ) {
+        countSame = countSame + 1;
+
+        arraySame.push(utentNumbers);
+
+    }
+    
+}
+    console.log(randomArray)  
+    console.log(utentNumbersArray);
+    console.log("i numeri uguali sono: " + countSame + " e sono i numeri seguenti: " + arraySame);
 
 
 
